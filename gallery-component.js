fe8c42d18b3cb0481d0a3abdffc1b50612a30be0
8443_cdbustamante <cdbustamante@espe.edu.ec>
2023-11-16 19:15:40 -0500

    cambio 2
    mejora de errores

--- gallery-component.js
+++ gallery-component.js
@@ -1,45 +1,44 @@
 
-// Definición del componente de galería de imágenes
 class ImageGallery extends HTMLElement {
     constructor() {
         super();
-
-        // Estado inicial
-        this.images = [
-            'images/image1.jpeg',
-            'images/image2.jpeg',
-            'images/image3.jpg',
-            'images/image4.jpg',
-            // Agrega más imágenes según sea necesario
-        ];
-        this.currentImageIndex = 0;
-
-        // Crea el shadow DOM
-        //this.attachShadow({ mode: 'open' });
-
-        // Renderiza la galería
-        this.render();
+        this.inicial();
     }
 
-    // Método para renderizar la galería
-    render() {
+    inicial() {
         this.innerHTML = `
-            <div class ="container-fluid">
+       <div class="carouselContainer">
             <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="images/image1.jpeg" class="d-block w-100 h-50 img-fluid" alt="...">
+                        <img src="images/image1.jpg" class="d-block  img-fluid" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/image2.jpeg" class="d-block w-100 img-fluid" alt="...">
+                        <img src="images/image2.jpg" class="d-block img-fluid" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/image3.jpg" class="d-block w-100 img-fluid" alt="...">
+                        <img src="images/image3.jpg" class="d-block  img-fluid" alt="...">
                     </div>
                     <div class="carousel-item">
                         <img src="images/image4.jpg" class="d-block img-fluid" alt="...">
                     </div>
                 </div>
+            </div>
+            
+            <div class="carousel-indicators" style="margin-bottom: -20px;">
+
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" style="width: 100px;">
+                    <img class="d-block w-100" src="images/image1.jpg" class="img-fluid" />
+                </button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="1" class="active" aria-current="true" aria-label="Slide 2" style="width: 100px;">
+                    <img class="d-block w-100" src="images/image2.jpg" class="img-fluid" />
+                </button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="2" class="active" aria-current="true" aria-label="Slide 3" style="width: 100px;">
+                    <img class="d-block w-100" src="images/image3.jpg" class="img-fluid" />
+                </button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="3" class="active" aria-current="true" aria-label="Slide 4" style="width: 100px;">
+                    <img class="d-block w-100" src="images/image4.jpg" class="img-fluid" />
+                </button>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
@@ -52,47 +51,7 @@ class ImageGallery extends HTMLElement {
             </div>
         `;
 
-        /*
-        // Agrega eventos a los botones
-        this.shadowRoot.getElementById('prevButton').addEventListener('click', () => this.showPrevImage());
-        this.shadowRoot.getElementById('nextButton').addEventListener('click', () => this.showNextImage());
-        */
-    }
-    /*
-    // Método para mostrar la imagen anterior
-    showPrevImage() {
-        this.currentImageIndex = (this.currentImageIndex - 1 + this.images.length) % this.images.length;
-        this.updateImage();
-    }
 
-    // Método para mostrar la siguiente imagen
-    showNextImage() {
-        this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-        this.updateImage();
     }
-
-    // Método para actualizar la imagen mostrada
-    updateImage() {
-        this.shadowRoot.querySelector('img').src = this.images[this.currentImageIndex];
-    }
-    */
 }
-
-// Define el nuevo elemento personalizado
 customElements.define('primer-carrusel', ImageGallery);
-
-/*
-render() {
-    this.shadowRoot.innerHTML = `
-        <div class="image-gallery">
-            <button class="button" id="prevButton">&lt;</button>
-            <img src="${this.images[this.currentImageIndex]}" alt="Gallery Image">
-            <button class="button" id="nextButton">&gt;</button>
-        </div>
-    `;
-
-    // Agrega eventos a los botones
-    this.shadowRoot.getElementById('prevButton').addEventListener('click', () => this.showPrevImage());
-    this.shadowRoot.getElementById('nextButton').addEventListener('click', () => this.showNextImage());
-}
-*/
